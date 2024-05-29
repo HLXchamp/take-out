952e17f25777e3f173aeba4cfe101cffcc63b604
HLXchamp <1871382623@qq.com>
2024-05-29 20:32:14 +0800

    根据状态统计订单数量

--- sky-take-out/sky-server/src/main/java/com/sky/controller/admin/OrderController.java
+++ sky-take-out/sky-server/src/main/java/com/sky/controller/admin/OrderController.java
@@ -4,7 +4,9 @@ import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.OrderService;
+import com.sky.vo.OrderStatisticsVO;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,9 +22,26 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
+    /**
+     * 分页查询订单
+     * @param ordersPageQueryDTO
+     * @return
+     */
     @GetMapping("/conditionSearch")
     public Result<PageResult> conditionSearch(OrdersPageQueryDTO ordersPageQueryDTO){
         PageResult pageResult = orderService.conditionSearch(ordersPageQueryDTO);
         return Result.success(pageResult);
+    }
+
+    /**
+     * 各个状态的订单数量统计
+     *
+     * @return
+     */
+    @GetMapping("/statistics")
+    @ApiOperation("各个状态的订单数量统计")
+    public Result<OrderStatisticsVO> statistics() {
+        OrderStatisticsVO orderStatisticsVO = orderService.statistics();
+        return Result.success(orderStatisticsVO);
     }
 }
